b9b583bd301ba4bf4b2dfe0b451da74ccecf3559
Sergey Kudashev <kudashevs@gmail.com>
2021-10-19 21:58:41 +0300

    Refactor "Which are in" move loop solution to separate file.

--- src/kata/kyu6/which_are_in/WhichAreInLoop.java
+++ src/kata/kyu6/which_are_in/WhichAreInLoop.java
@@ -2,7 +2,7 @@ package kyu6.which_are_in;
 
 import java.util.Arrays;
 
-public class WhichAreIn {
+public class WhichAreInLoop {
     public static String[] inArray(String[] array1, String[] array2) {
         return Arrays.stream(array1).filter((v) -> {
             for (String hay : array2) {
--- src/test/kyu6/which_are_in/WhichAreInLoopTest.java
+++ src/test/kyu6/which_are_in/WhichAreInLoopTest.java
@@ -4,12 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class WhichAreInTest {
+public class WhichAreInLoopTest {
 	@Test
 	public void test1() {
 		String a[] = new String[]{ "arp", "live", "strong" };
     	String b[] = new String[] { "lively", "alive", "harp", "sharp", "armstrong" };
     	String r[] = new String[] { "arp", "live", "strong" };
-		assertArrayEquals(r, WhichAreIn.inArray(a, b));
+		assertArrayEquals(r, WhichAreInLoop.inArray(a, b));
 	}
 }
